45855ad36382799ee4fdc2cbdfe9a684b56ab930
sixertoy <sixertoy.github@gmail.com>
2020-04-05 08:20:09 +0200

    :fix: prop-types import from index

--- src/prop-types.js
+++ src/prop-types.js
@@ -1,34 +1,28 @@
-import {
-  arrayOf,
-  bool,
-  node,
-  number,
-  oneOf,
-  oneOfType,
-  shape,
-  string,
-} from 'prop-types';
+import PropTypes from 'prop-types';
 
-export const PlacementType = oneOf(['top', 'bottom', 'both']);
+export const PlacementType = PropTypes.oneOf(['top', 'bottom', 'both']);
 
-export const ThemeType = shape({
-  color: string,
-  progressRadius: oneOfType([number, string]),
-  progressSize: oneOfType([number, string]),
-  titleFontSize: oneOfType([number, string]),
+export const ThemeType = PropTypes.shape({
+  color: PropTypes.string,
+  progressRadius: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  progressSize: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  titleFontSize: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 });
 
-export const TitleType = oneOfType([string, bool]);
+export const TitleType = PropTypes.oneOfType([
+  PropTypes.string,
+  PropTypes.bool,
+]);
 
-export const IconType = shape({
-  Checked: node.isRequired,
-  Unchecked: node.isRequired,
+export const IconType = PropTypes.shape({
+  Checked: PropTypes.node.isRequired,
+  Unchecked: PropTypes.node.isRequired,
 });
 
-export const TaskType = shape({
-  checked: bool.isRequired,
-  id: oneOfType([string, bool]).isRequired,
-  label: string.isRequired,
+export const TaskType = PropTypes.shape({
+  checked: PropTypes.bool.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]).isRequired,
+  label: PropTypes.string.isRequired,
 });
 
-export const TasksType = arrayOf(TaskType);
+export const TasksType = PropTypes.arrayOf(TaskType);
